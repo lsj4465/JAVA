3fe29b07b7e570b338ced22822564cbb67462242
Michael Seongjun Lee <lsj4465@gmail.com>
2019-01-29 19:51:14 +0900

    JAVA day 1(1번수정)

--- JAVA/Class/day1/src/Array_1.java
+++ JAVA/Class/day1/src/Array_1.java
@@ -3,7 +3,29 @@ public class Array_1 {
 
 	public static void main(String[] args) {
 		
-
+			int[] score = {76, 80, 55, 74, 81};
+			int sum = 0;
+			int aver = 0;
+			int max = score[0];
+			int min = score[0];
+			
+			for (int i = 0; i<score.length; i++ ) {
+				sum += score[i];
+			}
+			
+			aver = sum/5;
+			
+			for (int j = 0; j<score.length; j++) {
+				if (max<score[j]) {
+					max = score[j];
+					
+				}else if(min>score[j]) {
+					min = score[j];
+				}
+			}	
+			System.out.println("합 :" +sum);
+			System.out.println("평균 :" +aver);
+			System.out.println("최대 :" +max);
+			System.out.println("최소 :" +min);
+		}
 	}
-
-}
